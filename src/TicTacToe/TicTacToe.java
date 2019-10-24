410e7943a2de56b093214cb88d56fa7aa165ceae
gabelstapler6 <andreas.braun98@outlook.de>
2019-10-24 18:47:02 +0200

    Method createWindow

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -4,9 +4,27 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TicTacToe {
+    private JFrame frame;
+    private JPanel panel;
+
+    public TicTacToe() {
+        createWindow();    
+    }
+
+    public void createWindow(){
+        frame = new JFrame("TicTacToe");
+        frame.setSize(1024, 720);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+        panel.setBackground(Color.lightGray);
+
+        frame.add(panel);
+    }
 
     public static void main(String[] args) {
-        
+        new TicTacToe();
     }
 }
 
