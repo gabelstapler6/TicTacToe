d5193ee878a544fe34fcc3bb0fcfafa38b90ceb8
gabelstapler6 <andreas.braun98@outlook.de>
2019-10-25 12:26:37 +0200

    Gridlayout with JButton

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -2,19 +2,21 @@ package TicTacToe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class TicTacToe {
     private JFrame frame;
     private JPanel panel;
-    private JToggleButton[][] buttonArr = new JToggleButton[3][3];
+    private JButton[][] buttonArr = new JButton[3][3];
     private int buttonsize = 100;
+
     private int[][] player1Checker = new int[3][3];
     private int[][] player2Checker = new int[3][3];
 
     public TicTacToe() {
         createWindow();
         createButtons();
-       // play();
+        //play();
     }
 
     public void createWindow(){
@@ -26,41 +28,39 @@ public class TicTacToe {
         panel.setBackground(Color.WHITE);
         panel.setLayout(new GridLayout(3,3));
 
+    }
+
+    public void createButtons() {
         for(int z=0; z<3;z++){
             for(int i = 0; i < 3; i++){
-                buttonArr[i][z] = new JToggleButton();
+                buttonArr[i][z] = new JButton();
                 buttonArr[i][z].setSize(buttonsize, buttonsize);
                 buttonArr[i][z].setBackground(Color.GRAY);
                 panel.add(buttonArr[i][z]);
-            
             }
         }
-        frame.add(panel,"center");
+        frame.getContentPane().add(panel);
         frame.setSize(buttonsize*3, buttonsize*3);
         frame.setVisible(true);
     }
 
-    public void createButtons() {
-       
-    }
-
     public void play() {
         int state = 0;
-        while(state < 9)
-        for(int z = 0; z < 3; z++){
-            for(int i = 0; i < 3; i++){
-                if(buttonArr[i][z].isSelected()){
-                    switch(state % 2){
-                        case 0: buttonArr[i][z].setText("X");
-                        break;
-                        default: buttonArr[i][z].setText("O");
+        while(state < 9){
+            for(int z = 0; z < 3; z++){
+                for(int i = 0; i < 3; i++){
+                    if(buttonArr[i][z].isSelected()){
+                        switch(state % 2){
+                            case 0: buttonArr[i][z].setText("X");
+                            break;
+                            case 1: buttonArr[i][z].setText("O");
+                            break;
+                        }
+                        state++;
                     }
-                    state++;
                 }
             }
-        }
-
-        
+        }     
     }
 
     public static void main(String[] args) {
