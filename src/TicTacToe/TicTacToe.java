b3c3c5cd531ac5da688d3f8e226c278f14d477fe
gabelstapler6 <andreas.braun98@outlook.de>
2019-10-25 11:22:27 +0200

    play method

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -8,10 +8,13 @@ public class TicTacToe {
     private JPanel panel;
     private JToggleButton[][] buttonArr = new JToggleButton[3][3];
     private int buttonsize = 100;
+    private int[][] player1Checker = new int[3][3];
+    private int[][] player2Checker = new int[3][3];
 
     public TicTacToe() {
         createWindow();
-        createButtons();   
+        createButtons();
+        play();
     }
 
     public void createWindow(){
@@ -22,14 +25,12 @@ public class TicTacToe {
         frame.setResizable(false);
         panel = new JPanel();
         panel.setBackground(Color.WHITE);
-
+        
         frame.add(panel);
     }
 
-public void createButtons() {
-
+    public void createButtons() {
         int x = 100, y = 100;
-
         for(int z=0; z<3;z++){
             for(int i = 0; i < 3; i++){
                 buttonArr[i][z] = new JToggleButton();
@@ -41,6 +42,25 @@ public void createButtons() {
             y += buttonsize + 10;
             x = 100;
         }
+    }
+
+    public void play() {
+        int state = 0;
+        while(state < 9)
+        for(int z = 0; z < 3; z++){
+            for(int i = 0; i < 3; i++){
+                if(buttonArr[i][z].isSelected()){
+                    switch(state % 2){
+                        case 0: buttonArr[i][z].setText("X");
+                        break;
+                        default: buttonArr[i][z].setText("O");
+                    }
+                    state++;
+                }
+            }
+        }
+
+        
     }
 
     public static void main(String[] args) {
